4b6163658e39be5bd9b0e358b81a5f130e622372
Thiago de Oliveira Nascimento <veks@MacBook-Air-de-Thiago.local>
2026-03-18 21:40:20 -0300

    Ajuste: adicionado personagem 1 e 2 ao lado da foto de perfil da sessao sobre mim

--- app/_sections/About/about.tsx
+++ app/_sections/About/about.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
 
 import perfil from "../../public/perfil.png";
+import personagem1 from "../../public/personagem1.png";
+import personagem2 from "../../public/personagem2.png";
 
 const About = () => {
   return (
     <div className="w-screen py-32 bg-black flex flex-col justify-center items-center relative">
       <div className="absolute w-[150px] h-[150px] bg-[#A5D872] opacity-100 blur-[120px] rounded-full top-[30%] left-[70%] z-110"></div>
+      <Image
+        className="relative top-[600px] right-[160px] md:top-[600px] md:right-[160px] lg:top-[640px] lg:right-[450px]"
+        src={personagem1}
+      />
+      <Image
+        className="relative top-[170px] left-[170px] md:top-[170px] md:left-[170px] lg:top-[200px] lg:left-[-120px]"
+        src={personagem2}
+      />
       <h2 className="text-[#A5D872] font-bold text-3xl">Sobre Mim</h2>
       <h2 className="text-white font-semibold text-[18px]">
         Paixão, Criatividade, e Código
       </h2>
       <div className="mt-16">
         <div className="flex w-screen justify-center items-center gap-32 flex-wrap">
-          <Image src={perfil} width={300} height={300} />
+          <Image className="w-[300px]" src={perfil} />
           <div>
             <p className="w-[450px] text-white font-semibold ">
               Olá, eu sou o Thiago Desenvolvedor Frontend, Com um forte foco no
