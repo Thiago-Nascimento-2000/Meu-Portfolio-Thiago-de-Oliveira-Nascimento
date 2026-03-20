0b8650129fa5d4d9a6bc3bbc8ae2b69f2cbf72b3
Thiago de Oliveira Nascimento <veks@MacBook-Air-de-Thiago.local>
2026-03-20 00:53:18 -0300

    ajuste footer textos desktop e mobile

--- app/_sections/Footer/footer.tsx
+++ app/_sections/Footer/footer.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
   return (
     <div className="w-screen p-6  h-[150px] flex justify-center">
-      <div className="border-1 border-[#A5D872] rounded-[8px] w-[1388px] h-[110px] flex justify-between px-12 items-center">
+      <div className="border-1 border-[#A5D872] rounded-[8px] w-[1388px] h-[110px] flex flex-col md:flex-row p-2 justify-between px-12 items-center">
         <div>
-          <h2 className="text-white">
-            Desing e desenvolvimento feito por{" "}
-            <span className="text-[#A5D872]">Thiago O. Nascimento</span>
+          <h2 className="text-white flex flex-col lg:flex-row">
+            Desing e desenvolvimento feito por
+            <span className="text-[#A5D872] flex justify-center ml-1">
+              Thiago O. Nascimento
+            </span>
           </h2>
         </div>
         <div>
